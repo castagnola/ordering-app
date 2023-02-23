f6b853a50660e7b3381787d89e7d6cc6f4021e3e
castagnola <castagnola92@gmail.com>
2023-02-23 15:33:12 -0500

    Configure orders Repository

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schema/order.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './app/orders/.env',
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
